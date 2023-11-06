66966bd7061bf48bd857f7ab00eee5114df7b8e6
Muhammad Bilal Mehmood <i222452@nu.edu.pk>
2023-11-07 01:57:15 +0500

    Delete Testing.cpp

--- Testing.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    cout << "This is a test code just to check if my github desktop is working or not!!!" << endl;
-    //This comment is to check if my github desktop can update my cpp file in my repo!!
-    return 0;
-}
