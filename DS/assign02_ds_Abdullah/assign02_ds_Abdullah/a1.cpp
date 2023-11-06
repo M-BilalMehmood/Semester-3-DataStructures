56e6ce4c63fba61ac28a5664dcb5afa36cd9b2f4
Muhammad Bilal Mehmood <i222452@nu.edu.pk>
2023-11-07 01:30:51 +0500

    Codes of DS

--- DS/assign02_ds_Abdullah/assign02_ds_Abdullah/a1.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-#include<string>
-using namespace std;
-#include "a1.h"
-int main()
-{}
--- DS/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-
-int main()
-{
-    int r = 10;
-    printf("I am nigga %d times", r);
-    return 0;
-}
